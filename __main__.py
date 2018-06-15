2774c095cfbdd00faacdd537724783168cfd3041
hyucknaeKwon <nhk321@naver.com>
2018-06-15 14:36:44 +0900

    시각화 1

--- __main__.py
+++ __main__.py
@@ -12,22 +12,33 @@ if __name__=='__main__':
 
     #데이터 수집 (collection)
     for item in items:
-        collect.crawling(**item)
-        resultfile=collect.crawling(**item,fetch=False)
-        item['resultfile']=resultfile
+        resultfile = collect.crawling(**item, fetch=False)
+        item['resultfile'] = resultfile
         #item['resultfile']=resultfile
     #collect.crawling(
     ##    'jtbcnews',
      #   '2017-01-01',
      #   '2017-12-31'
     #)
+
+
+
     #데이터 분석(analyze)
+
     for item in items:
-        data=analyze.json_to_str(item['resultfile'], 'message')
-        print(data)
+        data = analyze.json_to_str(item['resultfile'], 'message')
+        item['count_wordfreq'] = analyze.count_wordfreq(data)
+
+        print(item['count_wordfreq'])
         #item['count_word_freq']=analyze.count_wordfreq(data)
 
     #데이터 시각화(visualize)
+    for item in items:
+        count= item['count_wordfreq']
+        count_m50=dict(count.most_common(50))
+
+        filename="%s_%s_%s" %(item['pagename'],item['since'],item['until'])
+        visualize.wordcloud(filename,count_m50)
 
 print('run analysis_fb...')
 
--- analyze/analyzer.py
+++ analyze/analyzer.py
@@ -1,28 +1,29 @@
 import json
 import re
-#분석을 위한 전처리
+from konlpy.tag import Twitter
+from collections import Counter
 
 
-#[a-zA-Z1-9]+ 1부터 Z까지 하나의 문자열?
-
-def json_to_str(filename,key):
-    jsonfile = open(filename,'r',encoding='utf-8')
-    json_string=jsonfile.read()
-
+def json_to_str(filename, key):
+    jsonfile = open(filename, 'r', encoding='utf-8')
+    json_string = jsonfile.read()
     jsonfile.close()
-    #객체기 때문에 리스트가 표시
-    data=''
 
-    json_data=json.loads(json_string)
-
-
+    data = ''
+    json_data = json.loads(json_string)
     for item in json_data:
-        value=item.get(key)
+        value = item.get(key)
         if value is None:
             continue
 
-        data+=re.sub('[^\w]]','',value)
+        data += re.sub(r'[^\w]', '', value)
 
     return data
 
-    print(json_data)
+
+def count_wordfreq(data):
+    twitter = Twitter()
+    nouns = twitter.nouns(data)
+
+    count = Counter(nouns)
+    return count
